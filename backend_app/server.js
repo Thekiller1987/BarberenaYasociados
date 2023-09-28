1dbf3c89f4006d1090cb4d2b9f4854063d5f5ace
Waskar <waskareliasrios11@gmail.com>
2023-09-27 21:27:38 -0600

    mi primer commit

---
+++ backend_app/server.js
@@ -0,0 +1,37 @@
+
+
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+// Middleware para analizar el cuerpo de la solicitud en formato JSON
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1987',
+    database: 'abogados_firma'
+
+});
+
+db.connect((err) =>{
+    if(err){
+        console.error('Error de conexion a la base de datos', err);
+    }else{
+        console.error('Conexion exitosa a la base de datos');
+    }
+});
+
+app.use(cors());
+
+app.listen(port,()=>{
+    console.log(`Servidor backend en funcionamiento en el puerto ${port}`);
+
+});
+
+const crudRoutes = require('./routes/crudRoutes.js')(db);
+app.use('/crud', crudRoutes);
