6dff11ef7cc0e04a6465acfbac2392c0508ca966
Oreki Houtaru <montenegrobrayan45@gmail.com>
2023-10-16 17:18:16 -0600

    frontend2

--- myfrontendapp/src/App.js
+++ myfrontendapp/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Customer from './pages/Customer';
+import Teacher from './pages/Teacher';
+import TeacherList from './pages/TeacherList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/teacher" element={<Teacher />} />
+        <Route path="/teacherList" element={<TeacherList />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ myfrontendapp/src/components/Header.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Offcanvas, Button, NavDropdown, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div>
+      {/* Navbar principal */}
+      <Navbar className="navbar-color" variant="dark" expand="md">
+        <Container>
+          <Navbar.Brand href="#home">Barberena y asociados</Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            style={{ display: 'none' }}
+            className="d-sm-none d-xs-none"
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+
+              <Nav.Link>
+                <Link to="/" className="link-unstyled">Inicio</Link>
+              </Nav.Link>
+
+              <Nav.Link>
+                <Link to="/about" className="link-unstyled">Informacion</Link>
+              </Nav.Link>
+
+              <NavDropdown title="Casos" id="Casos">
+                <NavDropdown.Item>
+                  <Link to="/customer" className="link-unstyled">Registrar Casos</Link>
+                </NavDropdown.Item>
+ 
+                <NavDropdown.Item>
+                  <Link to="/actualizar-caso" className="link-unstyled">Listar Casos</Link>
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown title="abogados" id="abogados">
+                <NavDropdown.Item>
+                  <Link to="/Teacher" className="link-unstyled">Registrar abogados</Link>
+                </NavDropdown.Item>
+
+                <NavDropdown.Item>
+                  <Link to="/abogList" className="link-unstyled">Listar abogados</Link>
+                </NavDropdown.Item>
+              </NavDropdown>
+
+            </Nav>
+          </Navbar.Collapse>
+          <Button
+            variant="outline-light"
+            onClick={toggleMenu}
+            className="d-md-none d-block"
+            aria-controls="basic-navbar-nav"
+            aria-expanded={showMenu ? 'true' : 'false'}
+          >
+            Menú
+          </Button>
+        </Container>
+      </Navbar>
+
+      {/* Menú lateral (Offcanvas) */}
+      <Offcanvas show={showMenu} onHide={toggleMenu} placement="start">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menú</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="flex-column">
+
+            <Nav.Link>
+              <Link to="/" className="link-unstyled">Inicio</Link>
+            </Nav.Link>
+
+            <Nav.Link>
+              <Link to="/about" className="link-unstyled">Informacion</Link>
+            </Nav.Link>
+
+            <NavDropdown title="Casos" id="casos">
+              <NavDropdown.Item>
+                <Link to="/customer" className="link-unstyled">Registrar Caso
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/actualizar-caso" className="link-unstyled">Listar Caso</Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="Abogados" id="abogados">
+              <NavDropdown.Item>
+                <Link to="/abog" className="link-unstyled">Registrar Abogados</Link>
+              </NavDropdown.Item>
+
+              <NavDropdown.Item>
+                <Link to="/abogList" className="link-unstyled">Listar abogados</Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+
+          </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
+  );
+}
+
+export default Header;
